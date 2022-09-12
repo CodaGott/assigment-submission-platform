04ca4f555cad48d002aaca0d9327bf4a2034d0f0
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-09-12 11:18:40 +0100

    updated readme

--- src/main/java/com/assignment/submission/portal/service/AssigmentService.java
+++ src/main/java/com/assignment/submission/portal/service/AssigmentService.java
@@ -22,9 +22,32 @@ public class AssigmentService {
     public Assignment createAssignment(User user) {
         Assignment assignment = new Assignment();
         assignment.setStatus(AssignmentStatus.PENDING_SUBMISSION.getStatus());
+        assignment.setNumber(findNextAssignmentToSubmit(user));
         assignment.setUser(user);
 
         return assigmentRepository.save(assignment);
+    }
+
+    private Integer findNextAssignmentToSubmit(User user) {
+        Set<Assignment> assignmentByUser = assigmentRepository.findByUser(user);
+
+        if (assignmentByUser ==null){
+            return 1;
+        }
+
+        Optional<Integer> nextAssignment = assignmentByUser.stream()
+                .sorted((assignment1, assignment2)
+                        ->{
+                    if (assignment1.getNumber() == null) return -1;
+                    if (assignment2.getNumber() == null) return -1;
+                    return assignment2.getNumber()
+                            .compareTo(assignment1.getNumber());
+                }).map(assignment ->{
+                    if (assignment.getNumber() == null) return null;
+                    return assignment.getNumber() + 1;
+                })
+                .findFirst();
+        return nextAssignment.orElse(1);
     }
 
     public Set<Assignment> findByUser(User user){
